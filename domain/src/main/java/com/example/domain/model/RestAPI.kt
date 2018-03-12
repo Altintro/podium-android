aac31925cb371d42a3557588362323bbce874fdb
Roldan Arellano <marta.roldan@atos.net>
2018-03-12 09:33:12 +0000

    Fixed minor bug in data model. Add models for games and tournaments services

--- domain/src/main/java/com/example/domain/model/RestAPI.kt
+++ domain/src/main/java/com/example/domain/model/RestAPI.kt
@@ -4,17 +4,32 @@ import java.io.Serializable
 
 
 data class ResponseAuth(val auth: Boolean,
-                    val token: String) : Serializable
+                        val token: String) : Serializable
+
+//-----------------------------------User-----------------------------------
 
 data class UserRegister(var name: String,
                         val alias: String,
                         val pass: String,
                         val email: String) : Serializable
 
-data class GameCreation(var vame: String)
-
 data class ResponseSearchUser(var success: Boolean,
                               var result: List<User>) : Serializable
 
+//-----------------------------------Game-----------------------------------
+
+data class GameCreation(var name: String)
+
 data class ResponseSearchGame(var success: Boolean,
                               var result: List<Game>) : Serializable
+
+//-----------------------------------Tournament-----------------------------------
+
+data class TournamentCreation(var name: String,
+                              var compType: String)
+
+data class ResponseSearchTournament(var success: Boolean,
+                                    var result: List<Tournament>) : Serializable
+
+
+
